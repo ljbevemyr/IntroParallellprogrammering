64607e221ae3959447c37d42087c3b81f0a88363
Lisa Bevemyr <libe9492@gullviva.it.uu.se>
2020-10-07 22:09:38 +0200

    borde funka...

--- assignment3/Game_Of_Life.c
+++ assignment3/Game_Of_Life.c
@@ -69,24 +69,85 @@ static void print_to_pgm(int ** array, int N, int t) {
 }
 #endif
 
+static void tick_thread(int T, int N, int ** current, int ** previous) {
+  int t, i, j, nbrs;
+  int thread_num = omp_get_thread_num();
+  int thread_tot = omp_get_num_threads();
+  int tot_elem = (N-2)*(N-2) / thread_tot; //så många element tråden ska räkna
+  int start_v, start_h, end_v, end_h;
+  /*int start_horizontal = ((thread_num * tot_elements) / N) + 1;
+  int start_vertical = ((thread_num * tot_elements) % N) + 1;
+  int end_horizontal, end_vertical;
+  if (thread_num == thread_tot) {
+    end_horizontal = N-1;
+    end_vertical = N-1;
+  }
+  else{
+    end_horizontal = ((thread_num+1) * tot_elements / N)+1;// ((0+1)*16/10)+1 = 2
+    end_vertical = ((thead_num+1) * tot_elements) % N;// ((0+1)*16%10) = 6
+    }*/
+
+  if (thread_num == 0) {
+    start_v = 1;
+    start_h = 1;
+    end_v = (tot_elem / (N-2)) + 1;
+    end_h = (tot_elem % (N-2));
+  }
+  else if (thread_num == thread_tot) {
+    start_v = (tot_elem*(thread_num) / (N-2)) + 1;
+    start_h = (tot_elem*(thread_num) % (N-2));
+    end_v = N-1;
+    end_h = N-1;
+  }
+  else {
+    start_v = (tot_elem*(thread_num) / (N-2)) + 1;
+    start_h = (tot_elem*(thread_num) % (N-2));
+    end_v = (tot_elem*(thread_num+1) / (N-2)) + 1;
+    end_h = (tot_elem*(thread_num+1) % (N-2));
+  }
+  for (t = 0 ; t < T ; t++) { //time steps
+    for (i = start_v ; i < end_v ; i++) //array dimensions
+      for (j = start_h ; j < end_h ; j++) { //array dimensions
+        nbrs = previous[i+1][j+1] + previous[i+1][j] + previous[i+1][j-1] \
+          + previous[i][j-1] + previous[i][j+1] \
+          + previous[i-1][j-1] + previous[i-1][j] + previous[i-1][j+1];
+        if (nbrs == 3 || ( previous[i][j]+nbrs == 3))
+          current[i][j] = 1;
+        else
+          current[i][j] = 0;
+      }
+    #ifdef OUTPUT
+    print_to_pgm(current, N, t+1);
+    #endif
+    //Swap current array with previous array
+    int ** swap = current;
+    current = previous;
+    previous = swap;
+    #pragma omp barrier
+  }
+
+}
+
 int main (int argc, char * argv[]) {
   int N; //array dimensions
   int T; //time steps
   int ** current, ** previous; //arrays - one for current timestep, one for previous timestep
-  int ** swap;//array pointer
-  int t, i, j, nbrs;//helper variables
+  //int ** swap;//array pointer
+  //int t, i, j, nbrs;//helper variables
+  int threads;
 
   double time;//variables for timing
   struct timeval ts,tf;
 
   /*Read input arguments*/
-  if (argc != 3) {
+  if (argc != 4) {
     fprintf(stderr, "Usage: ./exec ArraySize TimeSteps\n");
     exit(-1);
   }
   else {
     N = atoi(argv[1]);
     T = atoi(argv[2]);
+    threads = atoi(argv[3]);
   }
 
   /*Allocate and initialize matrices*/
@@ -102,9 +163,9 @@ int main (int argc, char * argv[]) {
   /*Game of Life*/
 
   gettimeofday(&ts,NULL);
-  for (t = 0 ; t < T ; t++) {
-    for (i = 1 ; i < N-1 ; i++)
-      for (j = 1 ; j < N-1 ; j++) {
+  /*for (t = 0 ; t < T ; t++) { //time steps
+    for (i = 1 ; i < N-1 ; i++) //array dimensions
+      for (j = 1 ; j < N-1 ; j++) { //array dimensions
         nbrs = previous[i+1][j+1] + previous[i+1][j] + previous[i+1][j-1] \
           + previous[i][j-1] + previous[i][j+1] \
           + previous[i-1][j-1] + previous[i-1][j] + previous[i-1][j+1];
@@ -122,7 +183,9 @@ int main (int argc, char * argv[]) {
     current = previous;
     previous = swap;
 
-  }
+    }*/
+
+  
   gettimeofday(&tf,NULL);
   time = (tf.tv_sec-ts.tv_sec)+(tf.tv_usec-ts.tv_usec)*0.000001;
 
